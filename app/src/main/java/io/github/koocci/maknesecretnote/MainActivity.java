75eb41f06faa7c88df6af4938df1a35ab883e93f
Koo jinhyun <lazyeffect1@gmail.com>
2018-08-27 14:53:16 +0900

    add search nearby market update

--- app/src/main/java/io/github/koocci/maknesecretnote/MainActivity.java
+++ app/src/main/java/io/github/koocci/maknesecretnote/MainActivity.java
@@ -1,15 +1,22 @@
 package io.github.koocci.maknesecretnote;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 
+import java.security.MessageDigest;
 import java.util.ArrayList;
 
 import io.github.koocci.maknesecretnote.Adapter.FoodMarketAdapter;
@@ -28,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, RegistActivity.class));
             }
         });
 
@@ -54,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
         FoodMarketAdapter adapter = new FoodMarketAdapter(this, R.layout.food_market_item, items);
 
         listView.setAdapter(adapter);
+
+        //getAppKeyHash();
     }
 
     @Override
@@ -63,18 +71,20 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void getAppKeyHash() {
+        //1vPV5o7VbVdsRAZNc1Aacdq2zTU=
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            Log.e("name not found", e.toString());
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
